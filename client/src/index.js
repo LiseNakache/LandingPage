1cf172f160596c6eb2e6281fc7fb8e39ee2733c9
Lise <nakache.lise@gmail.com>
2017-11-20 10:50:48 +0200

    first commit

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,6 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <img style={{width:300}}
-          src="https://raw.githubusercontent.com/webpack-contrib/awesome-webpack/master/media/awesome_webpack_branding.png"
-          alt="Webpack"/>
         <br/>
         Hello world!!
       </div>
